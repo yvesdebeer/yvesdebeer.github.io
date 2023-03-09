a070c1bc6a92456ba15f5472c86e9cd7fd736fb7
yvesdebeer <yves.debeer@gmail.com>
2023-03-09 19:14:42 +0100

    Update codeblock.js

--- assets/js/codeblock.js
+++ assets/js/codeblock.js
@@ -1,6 +1,5 @@
 var codeBlocks = document.querySelectorAll('pre.highlight');
 
-
 codeBlocks.forEach(function (codeBlock) {
   var  Button = document.createElement('button');
    Button.className = ' ';
@@ -8,18 +7,14 @@ codeBlocks.forEach(function (codeBlock) {
    Button.ariaLabel = 'Copy code to clipboard';
    Button.innerText = 'Copy';
 
-
   codeBlock.append( Button);
-
 
    Button.addEventListener('click', function () {
     var code = codeBlock.querySelector('code').innerText.trim();
     window.navigator.clipboard.writeText(code);
 
-
      Button.innerText = 'Copied';
     var fourSeconds = 4000;
-
 
     setTimeout(function () {
        Button.innerText = 'Copy';
